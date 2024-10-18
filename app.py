c1f5236ca5ccd2458043ae078e30f8862a6d37fe
Fabio Riva <f.riva@sotefin.ch>
2024-10-18 15:16:20 +0200

    add template + new video streaming route

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, Response
+import os
+import re
+from flask import Flask, Response, url_for, redirect, render_template, send_from_directory
 from camera_pi2 import Camera
 
 app = Flask(__name__)
@@ -7,8 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """Video streaming home page."""
-    return '<html><img src="/video_feed" width="480" height="360" /></html>'
-
+    return render_template('index.html')
 
 def gen(camera):
     """Video streaming generator function."""
@@ -17,12 +18,14 @@ def gen(camera):
         frame = camera.get_frame()
         yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
 
-
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route("/video/<filename>")
+def serve_video(filename):
+    return send_from_directory('media', filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
